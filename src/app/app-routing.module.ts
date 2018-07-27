9b9b11790dfdc4122b3747ba4cefc5b8a972df66
ChloeCacola <cscorrigan@icloud.com>
2018-07-26 22:25:55 -0400

    testing prod fixes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// pages
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import {UnderConstructionComponent} from "./under-construction/under-construction.component";
+
+const appRoutes: Routes = [
+  {path: '', component: UnderConstructionComponent },
+  {path: 'not-found', component: PageNotFoundComponent },
+  {path: '**', redirectTo:'/not-found'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    UnderConstructionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
